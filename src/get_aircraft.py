bc54cc7cc16a1076d4a6328f934343a17a42c04f
Ananth Arunkumar <ananth2.718@gmail.com>
2024-11-24 22:26:56 -0500

    started development of get_aircraft script to... get the aircraft

--- src/get_aircraft.py
+++ src/get_aircraft.py
@@ -1,1 +1,22 @@
 from opensky_api import OpenSkyApi
+import argparse
+
+parser = argparse.ArgumentParser()
+parser.add_argument(
+    "run_interval",
+    help="how often the API should be queried (seconds)",
+    type=int
+)
+parser.add_argument(
+    "run_time",
+    help="how long the program should run for (seconds)",
+    type=int
+)
+args = parser.parse_args()
+
+print(args.run_interval)
+
+api = OpenSkyApi()
+
+# s = api.get_states()
+# print(s.states[0])
