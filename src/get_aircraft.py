b6d5516447ddb85ec73fe869796ff2d66538280a
Ananth Arunkumar <ananth2.718@gmail.com>
2024-11-26 18:27:06 -0500

    more setup and env variables and requirements and api keys :)

--- src/get_aircraft.py
+++ src/get_aircraft.py
@@ -1,5 +1,19 @@
 from opensky_api import OpenSkyApi
+from dotenv import load_dotenv
 import argparse
+import os
+
+
+def get_env_var(var_name, var_type):
+    var_value = os.getenv(var_name)
+    if var_value is None:
+        raise KeyError(f"{var_type} environment variable not found. do you have a .env file or have you exported the correct variables?")
+    return var_value
+
+
+load_dotenv()
+username = get_env_var("OPENSKY_USERNAME", "username")
+password = get_env_var("OPENSKY_PASSWORD", "password")
 
 parser = argparse.ArgumentParser()
 parser.add_argument(
@@ -14,9 +28,7 @@ parser.add_argument(
 )
 args = parser.parse_args()
 
-print(args.run_interval)
+api = OpenSkyApi(username=username, password=password)
 
-api = OpenSkyApi()
-
-# s = api.get_states()
-# print(s.states[0])
+s = api.get_states()
+print(s.states[0])
