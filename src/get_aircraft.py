103fe28ead74abc1cfe905c0d00854043d803c29
Ananth Arunkumar <ananth2.718@gmail.com>
2024-11-26 22:13:56 -0500

    started database creation with sqlite3 :D

--- src/get_aircraft.py
+++ src/get_aircraft.py
@@ -1,6 +1,7 @@
 from opensky_api import OpenSkyApi
 from dotenv import load_dotenv
 import argparse
+import sqlite3
 import os
 
 
@@ -26,9 +27,48 @@ parser.add_argument(
     help="how long the program should run for (seconds)",
     type=int
 )
+parser.add_argument(
+    "db_path",
+    help="where to save the sqlite database (file path)",
+    type=str
+)
 args = parser.parse_args()
+
+con = sqlite3.connect(args.db_path)
+cur = con.cursor()
+
+info_table = """
+    CREATE TABLE aircraft_info(
+        icao CHAR(6),
+        callsign VARCHAR(15),
+        country VARCHAR(64),
+        aircraft_category INT
+    )
+"""
+cur.execute(info_table)
 
 api = OpenSkyApi(username=username, password=password)
 
 s = api.get_states()
-print(s.states[0])
+state_ls = [
+    (
+        state.icao24,
+        state.callsign,
+        state.origin_country,
+        state.category
+    )
+    for state in s.states
+]
+
+insert_aircraft = """
+    INSERT INTO aircraft_info (
+        icao,
+        callsign,
+        country,
+        aircraft_category
+    )
+    VALUES (?, ?, ?, ?);
+"""
+cur.executemany(insert_aircraft, state_ls)
+
+con.commit()
