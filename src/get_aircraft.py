8d3189d2a2664292f84961b0c39c984f54c70cef
Ananth Arunkumar <ananth2.718@gmail.com>
2025-03-07 21:53:10 -0500

    cleanup!!

--- src/get_aircraft.py
+++ src/get_aircraft.py
@@ -1,74 +1,79 @@
 from opensky_api import OpenSkyApi
 from dotenv import load_dotenv
+from time import time, sleep
 import argparse
-import sqlite3
 import os
 
 
-def get_env_var(var_name, var_type):
+def get_env_var(var_name):
     var_value = os.getenv(var_name)
     if var_value is None:
-        raise KeyError(f"{var_type} environment variable not found. do you have a .env file or have you exported the correct variables?")
+        raise KeyError(f"{var_name} environment variable not found. do you have a .env file or have you exported the correct variables?")
     return var_value
 
 
 load_dotenv()
-username = get_env_var("OPENSKY_USERNAME", "username")
-password = get_env_var("OPENSKY_PASSWORD", "password")
+username = get_env_var("OPENSKY_USERNAME")
+password = get_env_var("OPENSKY_PASSWORD")
 
 parser = argparse.ArgumentParser()
 parser.add_argument(
-    "run_interval",
-    help="how often the API should be queried (seconds)",
+    "run_length",
+    help="the length of time for which the api will be queried (HH:MM:SS)",
     type=int
 )
 parser.add_argument(
-    "run_time",
-    help="how long the program should run for (seconds)",
+    "run_cooldown",
+    help="how long the program should wait for before calling the api again (seconds)",
     type=int
 )
 parser.add_argument(
     "db_path",
-    help="where to save the sqlite database (file path)",
+    help="where to save the database (file path)",
     type=str
 )
 args = parser.parse_args()
 
-con = sqlite3.connect(args.db_path)
-cur = con.cursor()
+db = TinyDB(args.db_path)
 
-info_table = """
-    CREATE TABLE aircraft_info(
-        icao CHAR(6),
-        callsign VARCHAR(15),
-        country VARCHAR(64),
-        aircraft_category INT
-    )
-"""
-cur.execute(info_table)
+# info_table = """
+#     CREATE TABLE aircraft_info(
+#         icao CHAR(6) PRIMARY KEY,
+#         callsign VARCHAR(15),
+#         country VARCHAR(64),
+#         aircraft_category INT
+#     )
+# """
+# cur.execute(info_table)
 
 api = OpenSkyApi(username=username, password=password)
 
 s = api.get_states()
-state_ls = [
-    (
-        state.icao24,
-        state.callsign,
-        state.origin_country,
-        state.category
-    )
+query_time = time()
+state_info = [
+    {
+        "icao": state.icao24,
+        "callsign": state.callsign,
+        "country": state.origin_country,
+        "lat": state.latitude,
+        "lon": state.longitude,
+        "type": state.category,
+        "time": query_time
+    }
     for state in s.states
 ]
 
-insert_aircraft = """
-    INSERT INTO aircraft_info (
-        icao,
-        callsign,
-        country,
-        aircraft_category
-    )
-    VALUES (?, ?, ?, ?);
-"""
-cur.executemany(insert_aircraft, state_ls)
+print('\n'.join(state_info))
 
-con.commit()
+# insert_aircraft = """
+#     INSERT INTO aircraft_info (
+#         icao,
+#         callsign,
+#         country,
+#         aircraft_category
+#     )
+#     VALUES (?, ?, ?, ?);
+# """
+# cur.executemany(insert_aircraft, state_ls)
+
+# con.commit()
